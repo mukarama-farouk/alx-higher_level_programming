9129ba3043c3967bc7054f45be33facb550ef764
mukarama-farouk <faroukmukarama@gmail.com>
2023-06-08 11:08:23 -0700

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
 
-args = sys.argv[1:]
-args_len = len(args)
-if args_len == 0:
-    print("0 arguments.")
-elif args_len == 1:
-    print("{} argument:".format(args_len))
-else:
-    print("{} arguments:".format(args_len))
 
-for i in range(args_len):
-    print("{}: {}".format(i + 1, args[i]))
+    import sys
+
+    args = sys.argv[1:]
+    args_len = len(args)
+    if args_len == 0:
+        print("0 arguments.")
+    elif args_len == 1:
+        print("{} argument:".format(args_len))
+    else:
+        print("{} arguments:".format(args_len))
+
+    for i in range(args_len):
+        print("{}: {}".format(i + 1, args[i]))
