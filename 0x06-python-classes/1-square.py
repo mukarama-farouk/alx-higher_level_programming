8708b560976835382bf1fef89e1046909a34b26f
mukarama-farouk <faroukmukarama@gmail.com>
2023-06-28 11:44:52 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class Square that defines a square"""
+
+class Square:
+    """Class Square that defines a square"""
+    def __init__(self, size):
+        """Initialise square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
