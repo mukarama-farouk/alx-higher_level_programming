fe1242b26b29d376bf25556e49a5c9b117ba073f
mukarama-farouk <faroukmukarama@gmail.com>
2023-07-05 13:44:20 -0700

    task 3

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module contains a function that prints My name is <first name> <last name>"""
+
+
+def say_my_name(first_name, last_name=""):
+    """A function that print strings
+
+    args:
+        first_name: Must be string
+        last_name: Must be string
+
+    Raises:
+        TypeError: If first_name and last_name arent strings
+
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
