31061f90399d67b661aa0e5c477603d8befa806f
mukarama-farouk <faroukmukarama@gmail.com>
2023-07-11 01:27:07 -0700

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A module that writes a string to a file"""
+
+def write_file(filename="", text=""):
+    """This function writes a string to a text file
+
+    Args:
+        filename: filename
+        text: text to be written
+
+    Returns:
+        The number of characters written
+
+    Raises:
+        None
+
+    """
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
