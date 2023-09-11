5645d4976bb03f4f27ba9a6af3fe95f7a710b5d4
mukarama-farouk <faroukmukarama@gmail.com>
2023-09-11 09:28:10 -0700

    task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv[2];
+const num = parseInt(args);
+
+if (!isNaN(num) && Number.isFinite(num)) {
+	console.log(`My number: ${num}`);
+}
+else {
+	console.log('Not a number');
+}
