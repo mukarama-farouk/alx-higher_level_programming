fee1d1725e4ff586787d99b4b451fb2986b35f62
mukarama-farouk <faroukmukarama@gmail.com>
2023-09-11 04:48:28 -0700

    task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+let lineOne = 'C is fun';
+let lineTwo = 'Python is cool';
+let lineThree = 'JavaScript is amazing';
+
+console.log(lineOne);
+console.log(lineTwo);
+console.log(lineThree);
